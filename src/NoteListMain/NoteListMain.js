fe2e4147130fb1887e103ddde9f99404e76104f3
rhammock1 <rhamm1027@gmail.com>
2020-11-10 16:28:13 -0500

    Added NoteListMain component

---
+++ src/NoteListMain/NoteListMain.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Note from '../Note/Note';
+
+const NoteListMain = function(props) {
+  return (
+    <section className='NoteListMain'>
+      <ul>
+        {props.notes.map(note => {
+          <li key={note.id}>
+            <Note id={note.id} name={note.name} modified={note.modified} />
+          </li>
+        })}
+      </ul>
+      <button type='button'>Add Note</button>
+    </section>
+    )
+}
+NoteListMain.defaultProps = {
+  notes:[]
+}
+export default NoteListMain;
